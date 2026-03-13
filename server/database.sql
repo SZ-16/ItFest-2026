71af448f0111145888d9b84369089c5f5ea20851
ZbStefan <stefan.zbercea05@e-uvt.ro>
2026-03-13 23:05:31 +0200

    Database new table and localhost

--- server/database.sql
+++ server/database.sql
@@ -1,5 +1,16 @@
+DROP TABLE IF EXISTS site_scans;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE site_scans (
     id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id), -
     image_name VARCHAR(255) NOT NULL,
     has_anomaly BOOLEAN NOT NULL,
     ai_description TEXT,
--- server/src/main/kotlin/org/example/project/Application.kt
+++ server/src/main/kotlin/org/example/project/Application.kt
@@ -5,16 +5,43 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.request.*
+import io.ktor.http.content.*
+import java.io.File
 
 fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
     routing {
         get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
+            call.respondText("Serverul merge!")
+        }
+
+        post("/api/upload-frame") {
+            val multipartData = call.receiveMultipart()
+            var fileName = ""
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName ?: "imagine_santier.jpg"
+                        val fileBytes = part.streamProvider().readBytes()
+
+                        val uploadDir = File("uploads")
+                        if (!uploadDir.exists()) uploadDir.mkdir()
+
+                        File("uploads/$fileName").writeBytes(fileBytes)
+                        println("✅ Am primit și salvat poza: $fileName")
+                    }
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+            call.respondText("Poza $fileName a ajuns cu succes pe server!")
         }
     }
 }
