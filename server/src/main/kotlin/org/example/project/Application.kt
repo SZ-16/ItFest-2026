8f8e3eaa3f32f6267ab01b3e57cd74a9f65dfabe
ZbStefan <stefan.zbercea05@e-uvt.ro>
2026-03-14 03:52:26 +0200

    Made different files cause we did a shitty thing and we kinda fucked up what we did

--- server/src/main/kotlin/org/example/project/Application.kt
+++ server/src/main/kotlin/org/example/project/Application.kt
@@ -3,8 +3,6 @@ package org.example.project
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.websocket.*
-import java.time.Duration
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -12,11 +10,5 @@ fun main() {
 }
 
 fun Application.module() {
-    install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(15)
-        maxFrameSize = Long.MAX_VALUE
-        masking = false
-    }
     configureRouting()
 }
